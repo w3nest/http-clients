da5eda174e4989d89c6f34a5d7905b89c78a0112
youwol-guillaume <greinisch@youwol.com>
2024-11-20 15:09:47 +0100

    🚚 Reorganize `Local` module

--- src/lib/local/index.ts
+++ src/lib/local/index.ts
@@ -1,4 +1,9 @@
 export * from './local.client'
-export * as Routers from './routers'
+export * as Commands from './routers/commands'
+export * as Environment from './routers/environment'
+export * as Components from './routers/components'
+export * as Projects from './routers/projects'
+export * as System from './routers/system'
+export * as Python from './routers/python'
 
 export { LocalClient as Client } from './local.client'
